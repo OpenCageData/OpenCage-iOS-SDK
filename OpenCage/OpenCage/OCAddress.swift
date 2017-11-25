939e34363694b83c09ead1c60ada0ac28746a75e
Francis Beasley <fbeasleysoftware@gmail.com>
2017-11-25 22:06:52 +0000

    Finishing up the reverse geocode parser

--- OpenCage/OpenCage/OCAddress.swift
+++ OpenCage/OpenCage/OCAddress.swift
@@ -8,39 +8,22 @@
 
 import UIKit
 
+class OCBounds: NSObject {
+    var northeastLatitude :NSNumber!
+    var northeastLongitude :NSNumber!
+    var southwestLatitude :NSNumber!
+    var southwestLongitude :NSNumber!
+}
+
 class OCAddress: NSObject {
     
     var confidence :Int = 0
     var formattedAddress :String = ""
+    var latitude :NSNumber!
+    var longitude :NSNumber!
     
-    
-    /*
-    "bounds": {
-        "northeast": {
-            "lat": 51.5127636,
-            "lng": 0.1311587
-        },
-        "southwest": {
-            "lat": 51.512002,
-            "lng": 0.1276985
-        }
-    },
-    "components": {
-        "ISO_3166-1_alpha-2": "GB",
-        "_type": "road",
-        "city": "London",
-        "country": "United Kingdom",
-        "country_code": "gb",
-        "postcode": "SE28 8NZ",
-        "road": "St Andrews Close",
-        "state": "England",
-        "state_district": "Greater London"
-    },
-    "geometry": {
-        "lat": 51.5125159,
-        "lng": 0.1298424
-    }
- */
+    var bounds :OCBounds!
+    var component :OCComponent!
     var annotations :[OCAnnotation] = []
     
     func setupWithData(dictionary :NSDictionary) {
@@ -59,6 +42,63 @@ class OCAddress: NSObject {
         
         if let confidenceString :Int = dictionary.object(forKey: "confidence") as? Int {
             confidence = confidenceString
+        }
+        
+        if let geometryDict :NSDictionary = dictionary.object(forKey: "geometry") as? NSDictionary {
+            latitude = NSNumber(value:  geometryDict.object(forKey: "lat") as! Double)
+            longitude = NSNumber(value:  geometryDict.object(forKey: "lng") as! Double)
+        }
+        
+        bounds = OCBounds()
+        if let boundsDict :NSDictionary = dictionary.object(forKey: "bounds") as? NSDictionary {
+            if let northeastDict :NSDictionary = boundsDict.object(forKey: "northeast") as? NSDictionary {
+                bounds.northeastLatitude = NSNumber(value:  northeastDict.object(forKey: "lat") as! Double)
+                bounds.northeastLongitude = NSNumber(value:  northeastDict.object(forKey: "lng") as! Double)
+            }
+            
+            if let southwestDict :NSDictionary = boundsDict.object(forKey: "southwest") as? NSDictionary {
+                bounds.southwestLatitude = NSNumber(value:  southwestDict.object(forKey: "lat") as! Double)
+                bounds.southwestLongitude = NSNumber(value:  southwestDict.object(forKey: "lng") as! Double)
+            }
+        }
+        
+        component = OCComponent()
+        if let componentsDict :NSDictionary = dictionary.object(forKey: "components") as? NSDictionary {
+            if let cityString :String = componentsDict.object(forKey: "city") as? String {
+                component.city = cityString
+            }
+            
+            if let countryString :String = componentsDict.object(forKey: "country") as? String {
+                component.country = countryString
+            }
+            
+            if let postcodeString :String = componentsDict.object(forKey: "postcode") as? String {
+                component.postcode = postcodeString
+            }
+            
+            if let roadString :String = componentsDict.object(forKey: "road") as? String {
+                component.road = roadString
+            }
+            
+            if let stateString :String = componentsDict.object(forKey: "state") as? String {
+                component.state = stateString
+            }
+            
+            if let stateDistrictString :String = componentsDict.object(forKey: "state_district") as? String {
+                component.stateDistrict = stateDistrictString
+            }
+            
+            if let countryCodeString :String = componentsDict.object(forKey: "country_code") as? String {
+                component.countryCode = countryCodeString
+            }
+            
+            if let isoCodeString :String = componentsDict.object(forKey: "ISO_3166-1_alpha-2") as? String {
+                component.iso3166Code = isoCodeString
+            }
+            
+            if let typeString :String = componentsDict.object(forKey: "_type") as? String {
+                component.type = typeString
+            }
         }
     }
 }
--- OpenCage/OpenCageTests/OpenCageTests.swift
+++ OpenCage/OpenCageTests/OpenCageTests.swift
@@ -44,7 +44,19 @@ class OpenCageTests: XCTestCase {
     }
     
     func testRevereGeocoderPaid() {
+        let expectation = self.expectation(description: "fetch reverse geocode paid")
         
+        let ocSDK :OCSDK = OCSDK(apiKey: "cce72e47b0e14a8abcdf9d80f419d13b")
+        ocSDK.reverseGeocode(latitude: NSNumber(value: 51.5159), longitude: NSNumber(value: 0.1297), withAnnotations: true) { (response, success, error) in
+            if success {
+                if response.remainingLimit != nil { // Only Free Accounts have the rate limit
+                    XCTAssert(true, "Pass")
+                    expectation.fulfill()
+                }
+            }
+        }
+        
+        self.waitForExpectations(timeout: 10.0, handler: nil)
     }
     
     func testGeocoderErrors() {
